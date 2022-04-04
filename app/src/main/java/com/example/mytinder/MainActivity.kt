07d6c575463a9b879cd2b27d01db88d8abbc568c
yys7517 <yys7517@naver.com>
2022-04-04 18:55:25 +0900

    로그아웃, 유저 정보 가져오기 (프로필 이미지 미 구현)

--- app/src/main/java/com/example/mytinder/MainActivity.kt
+++ app/src/main/java/com/example/mytinder/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.mytinder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.ImageView
 import androidx.databinding.DataBindingUtil
+import com.example.mytinder.auth.IntroActivity
+import com.example.mytinder.auth.UserInfoModel
 import com.example.mytinder.databinding.ActivityMainBinding
 import com.example.mytinder.slider.CardStackAdapter
+import com.example.mytinder.utils.FirebaseRef
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.ktx.Firebase
 import com.yuyakaido.android.cardstackview.CardStackLayoutManager
 import com.yuyakaido.android.cardstackview.CardStackListener
 import com.yuyakaido.android.cardstackview.CardStackView
@@ -18,14 +30,27 @@ class MainActivity : AppCompatActivity() {
     private lateinit var cardStackAdapter: CardStackAdapter
     private lateinit var manager : CardStackLayoutManager
 
-    private val items = ArrayList<String>()
+    private lateinit var auth: FirebaseAuth
+
+    private val userDataList = ArrayList< UserInfoModel >()
+
+    private val TAG = MainActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView( this, R.layout.activity_main )
 
-        val cardStackView : CardStackView = binding.cardStackView
+        auth = Firebase.auth
 
+        val cardStackView : CardStackView = binding.cardStackView
+        val settingIcon : ImageView = binding.settingIcon
+
+        settingIcon.setOnClickListener {
+            auth.signOut()
+            val intent = Intent( this, IntroActivity::class.java )
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity( intent )
+        }
 
         manager = CardStackLayoutManager( baseContext, object : CardStackListener {
 
@@ -54,13 +79,60 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
-        items.add( "a" )
-        items.add( "b" )
-        items.add( "c" )
-
-        cardStackAdapter = CardStackAdapter( baseContext, items )
+        // 카드 스택 뷰에 연결하기.
+        cardStackAdapter = CardStackAdapter( baseContext, userDataList )
 
         cardStackView.adapter = cardStackAdapter
         cardStackView.layoutManager = manager
+
+        // 유저 데이터 리스트에 담아서 가져오기.
+        getUserDataList()
+
+    }
+
+    // 유저 데이터 카드 뷰에 가져오기
+    private fun getUserDataList() {
+
+        // Read from the database
+        FirebaseRef.userInfoRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                // 리스트에 값을 채우기 전에 리스트를 비워놓기
+                userDataList.clear()
+
+                // Log.d( "유저 리스트 가져오기", dataSnapshot.toString() )
+                /*
+                D/유저 리스트 가져오기: DataSnapshot { key = userInfo,
+                value = {
+                            H2fQDmUpIRZm2WxgarrnXc09TtZ2={uid=H2fQDmUpIRZm2WxgarrnXc09TtZ2, gender=M, city=1, nickname=123, age=2},
+                            9krD6iF8oNV2NLcNQYxB1QfI9Xl2={uid=9krD6iF8oNV2NLcNQYxB1QfI9Xl2, gender=m, city=1, nickname=1232, age=2},
+                            VceKMNlMFQgqSGaE7eO4anySg7y1={uid=VceKMNlMFQgqSGaE7eO4anySg7y1, gender=m, city=1, nickname=12, age=2}}
+                         }
+                 */
+
+                for( dataModel in dataSnapshot.children ) {
+
+                    val user = dataModel.getValue(UserInfoModel::class.java)
+
+                    /*
+                    val uid = user?.uid.toString()
+                    val nickname = user?.nickname.toString()
+                    val gender = user?.gender.toString()
+                    val city = user?.city.toString()
+                    val age = user?.age.toString()
+                     */
+
+                    userDataList.add( user!! )
+                }
+
+                cardStackAdapter.notifyDataSetChanged()
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+
+            }
+        })
     }
 }
--- app/src/main/java/com/example/mytinder/slider/CardStackAdapter.kt
+++ app/src/main/java/com/example/mytinder/slider/CardStackAdapter.kt
@@ -4,10 +4,13 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mytinder.R
+import com.example.mytinder.auth.UserInfoModel
 
-class CardStackAdapter( val context : Context, val items : List<String> ) : RecyclerView.Adapter< CardStackAdapter.ViewHolder > () {
+class CardStackAdapter( val context : Context, val items : List<UserInfoModel> ) : RecyclerView.Adapter< CardStackAdapter.ViewHolder > () {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardStackAdapter.ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate( R.layout.item_card, parent, false )
         return ViewHolder( view )
@@ -22,7 +25,18 @@ class CardStackAdapter( val context : Context, val items : List<String> ) : Recy
     }
 
     inner class ViewHolder( itemView : View) : RecyclerView.ViewHolder(itemView) {
-        fun binding( data : String ) {
+
+        val profileImageArea : ImageView = itemView.findViewById( R.id.profileImageArea )
+        val nicknameArea : TextView = itemView.findViewById( R.id.itemNickname )
+        val ageArea : TextView = itemView.findViewById( R.id.itemAge )
+        val cityArea : TextView = itemView.findViewById( R.id.itemCity )
+
+        fun binding( user : UserInfoModel ) {
+
+            nicknameArea.text = user.nickname
+            ageArea.text = user.age
+            cityArea.text = user.city
+
 
         }
     }
